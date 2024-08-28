cf803f6c0344285d965ef2638c17f01853ad4fcf
Ruslan Peshev <ruslan.peshev@pateplay.com>
2024-08-28 19:56:17 +0300

    A lot of FE improvements and logic added. Added prettier config & commands. Refactored smaller components in separate file (components.tsx).

---
+++ src/components.tsx
@@ -0,0 +1,122 @@
+import React, {useState} from "react";
+import {Grid, MenuItem, TextField} from "@mui/material";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+
+export const fontHeader = `"Roboto","Helvetica","Arial",sans-serif`;
+
+export interface LabelledTextFieldProps {
+    label: string;
+    width: string;
+    inputProps?: object;
+    value: string;
+    onChange: (event: React.ChangeEvent<{ value: unknown }>) => void;
+}
+
+export interface LabelledSelection {
+    label: string;
+    width: string;
+    options: string[];
+    value: string;
+    onChange: (event: React.ChangeEvent<{ value: unknown }>) => void;
+}
+
+export const LabelledTextField: React.FC<LabelledTextFieldProps> = ({label, width, inputProps, value, onChange}) => (
+    <Grid item xs={12} display="flex" flexDirection="row" alignItems="center" justifyContent="center">
+        <label style={{fontFamily: fontHeader, paddingRight: "10px"}}>{label}</label>
+        <TextField
+            id="outlined-basic"
+            variant="outlined"
+            style={{width, paddingRight: "10px"}}
+            inputProps={inputProps}
+            value={value}
+            onChange={onChange}
+            autoComplete="off"
+        />
+    </Grid>
+);
+
+export const LabeledSelection: React.FC<LabelledSelection> = ({label, width, options, value, onChange}) => (
+    <Grid item xs={12} display="flex" flexDirection="row" alignItems="center" justifyContent="center">
+        <label style={{fontFamily: fontHeader, paddingRight: "10px"}}>{label}</label>
+        <TextField
+            id="outlined-select"
+            select
+            variant="outlined"
+            style={{width, paddingRight: "10px"}}
+            onChange={onChange}
+            value={value}
+        >
+            {options.map((option) => (
+                <MenuItem key={option} value={option}>
+                    {option}
+                </MenuItem>
+            ))}
+        </TextField>
+    </Grid>
+);
+
+export interface TrainerFieldProps {
+    name: string;
+    inputProps: object;
+}
+
+export const TrainerField: React.FC<TrainerFieldProps> = ({name, inputProps}) => {
+    const [checked, setChecked] = useState(false);
+
+    const handleCheckboxChange = () => {
+        setChecked(!checked);
+    };
+
+    return (
+        <Grid item xs={12} display="flex" flexDirection="row" alignItems="center">
+            <Grid item xs={6} style={{paddingRight: "10px"}}>
+                <FormControlLabel
+                    control={<Checkbox checked={checked} onChange={handleCheckboxChange}/>}
+                    label={<span style={{textDecoration: checked ? 'none' : 'line-through'}}>{name}</span>}
+                />
+            </Grid>
+            <Grid item xs={3} style={{padding: "10px"}}>
+                <TextField
+                    id="outlined-basic"
+                    variant="outlined"
+                    style={{width: '50px', backgroundColor: checked ? 'white' : 'gainsboro'}}
+                    inputProps={inputProps}
+                    disabled={!checked}
+                    autoComplete="off"
+                />
+            </Grid>
+            <Grid item xs={3}></Grid>
+        </Grid>
+    );
+};
+
+export const TrainerField2: React.FC<TrainerFieldProps> = ({name, inputProps}) => {
+    const [checked, setChecked] = useState(false);
+
+    const handleCheckboxChange = () => {
+        setChecked(!checked);
+    };
+
+    return (
+        <Grid item xs={12} display="flex" flexDirection="row" alignItems="center">
+            <Grid item xs={3}></Grid>
+            <Grid item xs={6} style={{paddingRight: "10px"}}>
+                <FormControlLabel
+                    control={<Checkbox checked={checked} onChange={handleCheckboxChange}/>}
+                    label={<span style={{textDecoration: checked ? 'none' : 'line-through'}}>{name}</span>}
+                />
+            </Grid>
+            <Grid item xs={3} style={{padding: "10px"}}>
+                <TextField
+                    id="outlined-basic"
+                    variant="outlined"
+                    style={{width: '50px', backgroundColor: checked ? 'white' : 'gainsboro'}}
+                    inputProps={inputProps}
+                    disabled={!checked}
+                    autoComplete="off"
+                />
+            </Grid>
+        </Grid>
+    );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
+import React, {useEffect, useState} from 'react';
+import ReactDOM from 'react-dom/client';
 import {calcStatistics, generateExportText} from "./Statistics";
 import {Button, Divider, Grid, Stack, TextField} from "@mui/material";
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
 // @ts-ignore
 import Logo from "./assets/logo1.svg";
+import {fontHeader, LabeledSelection, LabelledTextField, TrainerField, TrainerField2} from "./components";
 
 declare global {
     interface Process {
@@ -19,63 +18,48 @@ declare global {
 }
 
 const isElectron = () => typeof window !== 'undefined' && window.process && window.process.type === 'renderer';
-
-const fontHeader = `"Roboto","Helvetica","Arial",sans-serif`;
-
-interface LabelledTextFieldProps {
-    label: string;
-    width: string;
-    inputProps?: object;
-}
-
-const LabelledTextField: React.FC<LabelledTextFieldProps> = ({label, width, inputProps}) => (
-    <Grid item xs={12} display="flex" flexDirection="row" alignItems="center" justifyContent="center">
-        <label style={{fontFamily: fontHeader, paddingRight: "10px"}}>{label}</label>
-        <TextField id="outlined-basic" variant="outlined" style={{width, paddingRight: "10px"}} inputProps={inputProps}/>
-    </Grid>
-);
-
-interface TrainerFieldProps {
-    name: string;
-}
-
-const TrainerField: React.FC<TrainerFieldProps> = ({name}) => (
-    <Grid item xs={12} display="flex" flexDirection="row" alignItems="center">
-        <Grid item xs={6} style={{paddingRight: "10px"}}>
-            <FormControlLabel control={<Checkbox checked={false} onChange={() => {
-            }}/>} label={name}/>
-        </Grid>
-        <Grid item xs={3} style={{padding: "10px"}}>
-            <TextField id="outlined-basic" variant="outlined" style={{width: '50px'}}/>
-        </Grid>
-        <Grid item xs={3}></Grid>
-    </Grid>
-);
-
-const TrainerField2: React.FC<TrainerFieldProps> = ({name}) => (
-    <Grid item xs={12} display="flex" flexDirection="row" alignItems="center">
-        <Grid item xs={3}></Grid>
-        <Grid item xs={6} style={{paddingRight: "10px"}}>
-            <FormControlLabel control={<Checkbox checked={false} onChange={() => {
-            }}/>} label={name}/>
-        </Grid>
-        <Grid item xs={3} style={{padding: "10px"}}>
-            <TextField id="outlined-basic" variant="outlined" style={{width: '50px'}}/>
-        </Grid>
-    </Grid>
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const TrainerNames: string[] = ['Алекс', 'Йоан', 'Калоян', 'Кико', 'Марио', 'Пеца', 'Рус', 'Стела'];
 
 const App: React.FC = () => {
     const [result, setResult] = useState('');
     const username = 'RusHrus';
 
-    const allMoney = 5764;
-    const trainingDays = 9;
+    const [selectedAuthor, setSelectedAuthor] = useState('');
+    const [allMoney, setAllMoney] = useState<number>(0);
+    const [trainingDays, setTrainingDays] = useState<number>(0);
+
+    const handleSelectChange = (event: React.ChangeEvent<{ name?: string; value: unknown }>) => {
+        setSelectedAuthor(event.target.value as string);
+    };
+
+    const handleMoneyChange = (event: React.ChangeEvent<{ name?: string; value: unknown }>) => {
+        setAllMoney(parseInt(event.target.value as string));
+    };
+
+    const handleTrainingsCountChange = (event: React.ChangeEvent<{ name?: string; value: unknown }>) => {
+        setTrainingDays(parseInt(event.target.value as string));
+    };
+
+    // const allMoney = 5764;
+    // const trainingDays = 9;
     const trainingsPerTrainer = {
         mario: 6,
         yoan: 3,
         rus: 9,
     }
+
+    useEffect(() => {
+        console.log('Selected author: ', selectedAuthor);
+    }, [selectedAuthor]);
+
+    useEffect(() => {
+        console.log('All money: ', allMoney);
+    }, [allMoney]);
+
+    useEffect(() => {
+        console.log('Training days: ', trainingDays);
+    }, [trainingDays]);
 
     const calculateAndGenerateExport = async () => {
         if (isElectron()) {
@@ -119,15 +103,31 @@ const App: React.FC = () => {
 
                             <Grid item xs={12} display="flex" flexDirection="row" style={{padding: "20px"}}>
                                 <Grid item xs={4}>
-                                    <LabelledTextField label="Изготвил:" width="150px" inputProps={{maxLength: 25}}/>
+                                    <LabeledSelection
+                                        label={'Изготвил:'}
+                                        width={'150px'}
+                                        options={TrainerNames}
+                                        value={selectedAuthor}
+                                        onChange={handleSelectChange}
+                                    />
                                 </Grid>
                                 <Grid item xs={4}>
-                                    <LabelledTextField label="Пари:" width="100px"
-                                                       inputProps={{onKeyPress: handleKeyPress, maxLength: 6}}/>
+                                    <LabelledTextField
+                                        label="Пари:"
+                                        width="100px"
+                                        inputProps={{onKeyPress: handleKeyPress, maxLength: 6}}
+                                        value={allMoney ? allMoney.toString() : ''}
+                                        onChange={handleMoneyChange}
+                                    />
                                 </Grid>
                                 <Grid item xs={4}>
-                                    <LabelledTextField label="Тренировки:" width="50px"
-                                                       inputProps={{onKeyPress: handleKeyPress, maxLength: 2}}/>
+                                    <LabelledTextField
+                                        label="Тренировки:"
+                                        width="50px"
+                                        inputProps={{onKeyPress: handleKeyPress, maxLength: 2}}
+                                        value={trainingDays ? trainingDays.toString() : ''}
+                                        onChange={handleTrainingsCountChange}
+                                    />
                                 </Grid>
                             </Grid>
 
@@ -138,19 +138,31 @@ const App: React.FC = () => {
                                 <label style={{fontWeight: "bold", fontFamily: fontHeader}}>Треньори:</label>
                             </Grid>
 
-                            <Grid item xs={12} display="flex" flexDirection="row" style={{padding: "20px", paddingTop: "0px"}}>
+                            <Grid item xs={12} display="flex" flexDirection="row"
+                                  style={{padding: "20px", paddingTop: "0px"}}>
                                 <Grid item xs={6} display="flex" flexDirection="column">
-                                    <TrainerField name={'Алекс'}/>
-                                    <TrainerField name={'Йоан'}/>
-                                    <TrainerField name={'Калоян'}/>
-                                    <TrainerField name={'Кико'}/>
+                                    <TrainerField name={'Алекс'}
+                                                  inputProps={{onKeyPress: handleKeyPress, maxLength: 2}}/>
+                                    <TrainerField name={'Йоан'}
+                                                  inputProps={{onKeyPress: handleKeyPress, maxLength: 2}}/>
+                                    <TrainerField name={'Калоян'}
+                                                  inputProps={{onKeyPress: handleKeyPress, maxLength: 2}}/>
+                                    <TrainerField name={'Кико'}
+                                                  inputProps={{onKeyPress: handleKeyPress, maxLength: 2}}/>
+                                    <TrainerField name={'Марио'}
+                                                  inputProps={{onKeyPress: handleKeyPress, maxLength: 2}}/>
                                 </Grid>
                                 <Divider orientation="vertical" flexItem/>
                                 <Grid item xs={6} display="flex" flexDirection="column">
-                                    <TrainerField2 name={'Марио'}/>
-                                    <TrainerField2 name={'Пеца'}/>
-                                    <TrainerField2 name={'Рус'}/>
-                                    <TrainerField2 name={'Стели'}/>
+                                    <TrainerField2 name={'Марто'}
+                                                   inputProps={{onKeyPress: handleKeyPress, maxLength: 2}}/>
+                                    <TrainerField2 name={'Пеца'}
+                                                   inputProps={{onKeyPress: handleKeyPress, maxLength: 2}}/>
+                                    <TrainerField2 name={'Рус'}
+                                                   inputProps={{onKeyPress: handleKeyPress, maxLength: 2}}/>
+                                    <TrainerField2 name={'Стели'}
+                                                   inputProps={{onKeyPress: handleKeyPress, maxLength: 2}}/>
+                                    <Grid item xs={12} display="flex" flexDirection="row" alignItems="center"/>
                                 </Grid>
                             </Grid>
 
@@ -181,7 +193,7 @@ const App: React.FC = () => {
                                         id="outlined-basic"
                                         variant="outlined"
                                         value="Тука ще се показва дали е успешно или не"
-                                        InputProps={{ readOnly: true }}
+                                        InputProps={{readOnly: true}}
                                         sx={{width: '-webkit-fill-available'}}
                                     />
                                 </Grid>
@@ -200,4 +212,4 @@ const App: React.FC = () => {
     );
 };
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+root.render(<App/>);
